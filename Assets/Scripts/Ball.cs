d4f4782710bdb0387514cd4ee83e4762e9cf85f0
STomsin <yodpat.k@gmail.com>
2026-08-21 03:02:01 +0700

    Event system

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,16 +1,92 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Ball : MonoBehaviour
+public enum BallColor
 {
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
+public class Balls : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private int point;
+
+    public int Point { get { return point; } set { point = value; } }
+
+    [SerializeField]
+    private BallColor color;
+
+    [SerializeField]
+    private MeshRenderer rd;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log(point);
+        GameManager.instance.PlayerScore += point;
+        Destroy(gameObject);
+    }
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetColorAndPoint(BallColor color)
+    {
+        switch (color)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = Color.pink;
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = new Color32(255, 255, 255, 255);
+                break;
+
+        }
+
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int playerScore;
+    public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
+
+    [SerializeField]
+    private GameObject[] ballPositions;
+
+    [SerializeField]
+    private GameObject ballPrefab;
+
+    [SerializeField]
+    private GameObject cubeBall;
+
+    [SerializeField]
+    private float xInput = 0f;
+
+    [SerializeField]
+    private GameObject ballline;
+
+    [SerializeField]
+    private GameObject cam;
+
+    [SerializeField]
+    private TMP_Text notiText;
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        RotateBall();
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            ShootBall();
+        }
+
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            xInput = 0.1f;
+        }
+        else if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            xInput = -0.1f;
+        }
+        else
+        {
+            xInput = 0f;
+        }
+
+        if (Keyboard.current.backspaceKey.wasPressedThisFrame)
+        {
+            StopBall();
+        }
+
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+        GameObject obj = Instantiate(ballPrefab,
+                    ballPositions[i].transform.position,
+                    Quaternion.identity);
+        Balls b = obj.GetComponent<Balls>();
+        b.SetColorAndPoint(col);
+    }
+
+    private void ShootBall()
+    {
+        Rigidbody rb = cubeBall.GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+        ballline.SetActive(false);
+        cam.transform.parent = null;
+        cam.transform.position = new Vector3(0, 50, 0);
+        cam.transform.eulerAngles = new Vector3(90, 90, 0);
+    }
+
+    private void RotateBall()
+    {
+        if (cubeBall != null)
+        {
+            if (cubeBall != null)
+                cubeBall.transform.Rotate(0f, xInput, 0f);
+        }
+    }
+
+    private void StopBall()
+    {
+        Rigidbody rb = cubeBall.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cubeBall.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        ballline.SetActive(true);
+        CameraBehindCubeBall();
+    }
+
+    private void CameraBehindCubeBall()
+    {
+        cam.transform.parent = cubeBall.transform;
+        cam.transform.position = cubeBall.transform.position + new Vector3(0f, 7f, -15f);
+        cam.transform.eulerAngles = new Vector3(30f, 0f, 0f);
+    }
+
+    public void ShowNotiText(int n)
+    {
+        playerScore += n;
+        notiText.text = $"This ball:{n}\nTotal Score is {playerScore}";
+    }
+
+    public void ShowString()
+    {
+        notiText.text = "You lose";
+    }
+}
