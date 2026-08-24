fddf72bd013d9a7003debf5de7e6ba6a6c989f06
STomsin <yodpat.k@gmail.com>
2026-08-24 17:31:42 +0700

    Rotate ball2

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Balls b = other.GetComponent<Balls>();
+
+        if (b != null)
+        {
+            if (b.Point == 0)
+            {
+                GameManager.instance.ShowString();
+                Time.timeScale = 0f;
+                return;
+            }
+
+            GameManager.instance.ShowNotiText(b.Point);
+            Destroy(b.gameObject);
+        }
+    }
+}
