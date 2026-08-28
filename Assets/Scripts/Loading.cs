b9a4c93169eee46e8122d0bd77dd66bfe593ab47
STomsin <yodpat.k@gmail.com>
2026-08-29 00:18:53 +0700

    Loading

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+
+    [SerializeField]
+    private float waitSeconds = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitSeconds > 0f)
+            waitSeconds -= Time.deltaTime;
+        else
+            StartCoroutine(LoadNewScene());
+    }
+
+    private IEnumerator LoadNewScene()
+    {
+        AsyncOperation oper = SceneManager.LoadSceneAsync("Scene01");
+
+        while (!oper.isDone)
+        {
+            slider.value = oper.progress / 0.9f;
+            yield return null;
+        }
+    }
+}
